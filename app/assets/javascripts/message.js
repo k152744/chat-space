b56251dd9db72faee6ebf646433e67e8d681dc62
k152744 <yasukiyasuki1@icloud.com>
2019-07-25 16:28:19 +0900

    edit_automatic

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -18,16 +18,12 @@ $(function(){
     return html;
   }
 
-  
-
-
   function scroll(){
     $('.messages').animate({scrollTop:$('.messages')[0].scrollHeight});
   }
 
   $(function(){
     setInterval(reloadMessages, 10000);
-    
   });
 
   function reloadMessages(){
@@ -40,33 +36,19 @@ $(function(){
         message: {id: message_id}
       },
       dataType:"json"
-
     })
 
     .done(function(message_contents){
-      console.log('success');
-
       $.each(message_contents,function(i, message_contents){
-
         var html = buildHTML(message_contents);
         $(".messages").append(html);
       })
-     
       scroll();
-    })
-    .fail(function(){
-      console.log('error');
-
     })
   }
 
-
-
   $("#new_message").on("submit",function(e) {
- 
     e.preventDefault();
-   
-
     var formData = new FormData(this);
     var url = $(this).attr("action");
     
@@ -78,8 +60,7 @@ $(function(){
       processData:false,
       contentType:false
     })
-   
-    
+
     .done(function(message_contents){
       var html = buildHTML(message_contents);
       $(".messages").append(html);
@@ -87,8 +68,8 @@ $(function(){
       $("#message_text").val("");
       scroll();
       $(".text-message")[0].reset();
-      
     })
+    
     .fail(function(){
       alert("error");
       $('.submit-box').prop('disabled', false);
